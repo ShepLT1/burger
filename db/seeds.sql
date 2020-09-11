f2c66ea34f0d738408c9f403075a1e8e53916c5d
ShepLT1 <lshepherd234@gmail.com>
2020-09-11 15:47:09 -0700

    Added config & db folders. Added packagee.json file. Added schema and seeds sql files inside db folder. Added connection js file in config folder. Created server js file in root

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burger (burger_name, devoured) VALUES ("Matty's Patty", true);
+INSERT INTO burger (burger_name, devoured) VALUES ("Bean City Burger", false);
+INSERT INTO burger (burger_name, devoured) VALUES ("Leaning Tower of Cheesa Burger", true);
