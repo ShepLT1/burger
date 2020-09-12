409ccffe6fa3bba288767c6b0e2ab2b34777919c
ShepLT1 <lshepherd234@gmail.com>
2020-09-11 17:13:33 -0700

    Added burger controller js file and wrote api routes

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,21 @@
 const orm = require("../config/orm");
 
-orm.selectAll("burgers");
+const burger = {
+    all: function(cb) {
+        orm.selectAll("burgers", function(data) {
+            cb(data);
+        });
+    },
+    create: function(table, res, cb) {
+        orm.insertOne("burgers", res, function(data) {
+            cb(data);
+        });
+    },
+    update: function(table, devoured, id, cb) {
+        orm.updateOne("burgers", res.devoured, res.id, function(data) {
+            cb(data);
+        });
+    }
+}
 
-orm.insertOne("burgers", res);
-
-orm.updateOne("burgers", res.devoured, res.id);
+module.exports = burger;
